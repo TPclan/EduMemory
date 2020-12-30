b432bc5c4cf3027b7f0df35f8b51fb95b88c74a6
Tom Fawcett <tomfawcett64@gmail.com>
2020-12-29 18:54:46 -0800

    added test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./styles/app.scss";
 
 function App() {
   const [pageStatus, setPageStatus] = useState(0);
-  return (
+  return ( test
     <div className="App">
       <Home pageStatus={pageStatus} setPageStatus={setPageStatus} />
       <Game pageStatus={pageStatus} />
