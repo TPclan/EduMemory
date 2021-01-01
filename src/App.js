3277a281161197b3e9914c9fedd1674d27ecfa54
Shawn <shawnfawcett@gmail.com>
2020-12-31 16:16:27 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -11,10 +11,31 @@ function App() {
   const [pageStatus, setPageStatus] = useState(0);
   return (
     <div className="App">
-      <Home pageStatus={pageStatus} setPageStatus={setPageStatus} />
-      <Game pageStatus={pageStatus} />
-      <Myp />
-      <Tutorial />
+      <section>
+        {(() => {
+          switch (pageStatus) {
+            case 1:
+              return (
+                <Game pageStatus={pageStatus} setPageStatus={setPageStatus} />
+              );
+            case 2:
+              return (
+                <Tutorial
+                  pageStatus={pageStatus}
+                  setPageStatus={setPageStatus}
+                />
+              );
+            case 3:
+              return (
+                <Myp pageStatus={pageStatus} setPageStatus={setPageStatus} />
+              );
+            default:
+              return (
+                <Home pageStatus={pageStatus} setPageStatus={setPageStatus} />
+              );
+          }
+        })()}
+      </section>
     </div>
   );
 }
