58200c5bb2ff066ef578c842449d57b2e2e4df8e
Shawn <shawnfawcett@gmail.com>
2021-01-05 18:22:04 -0800

    game setup

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-const Game = ({ pageStatus }) => {
+const Game = ({ setPageStatus, pageStatus }) => {
   return (
     <div className={`game ${pageStatus ? "active-game" : ""}`}>
+      <button className="home-button" onClick={() => setPageStatus(0)}>
+        Go Home
+      </button>
       <h1>EduMemory</h1>
       <h2>Game</h2>
     </div>
