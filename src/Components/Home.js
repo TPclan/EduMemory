02ddb9ed17fc06ab17af0298c83ce5e8f759b3b5
Shawn <shawnfawcett@gmail.com>
2021-01-11 15:06:45 -0800

    logo test

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,14 @@ import React from "react";
 //Temporarily disable logo. throwing error when going live
 // Need to change this back later: <img alt="EduMemory logo" src={logo}></img>
 //import logo from "../icons/EduLogo.ico";
+import logo from "../icons/brain-2.svg";
 
 const Home = ({ setPageStatus, pageStatus }) => {
   return (
     <div className="home">
       <div className="head">
         <h1>EduMemory</h1>
-        {/* <img alt="EduMemory logo" src={logo}></img> */}
+        <img alt="EduMemory logo" src={logo}></img>
       </div>
       <button onClick={() => setPageStatus(1)}>Play</button>
       <button onClick={() => setPageStatus(2)}>How To Play</button>
