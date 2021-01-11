82f329a5c3bdf7f6e8dde38f624b0fb09d1dc621
Shawn <shawnfawcett@gmail.com>
2021-01-11 13:32:27 -0800

    ico issue trying to fix

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 //Import Images or Logos
+//Temporarily disable logo. throwing error when going live
+// Need to change this back later: <img alt="EduMemory logo" src={logo}></img>
 import logo from "../icons/EduLogo.ico";
 
 const Home = ({ setPageStatus, pageStatus }) => {
@@ -7,7 +9,7 @@ const Home = ({ setPageStatus, pageStatus }) => {
     <div className="home">
       <div className="head">
         <h1>EduMemory</h1>
-        <img alt="EduMemory logo" src={logo}></img>
+        {/* <img alt="EduMemory logo" src={logo}></img> */}
       </div>
       <button onClick={() => setPageStatus(1)}>Play</button>
       <button onClick={() => setPageStatus(2)}>How To Play</button>
