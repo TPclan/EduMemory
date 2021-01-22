dfbcd1d5448918c330692c93ea92e678b4df3443
Tom Fawcett <tomfawcett64@gmail.com>
2021-01-21 16:24:43 -0800

    changed ico to svg

--- src/Components/Tutorial.js
+++ src/Components/Tutorial.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import logo from "../icons/EduLogo.ico";
+import logo from "../icons/brain-2.svg";
 import HomeButton from "../img/home-button.svg";
 
 const Tutorial = ({ setPageStatus }) => {
