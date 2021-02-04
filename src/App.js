819dac2a3c9ab92513896ea730fbb99e9e617f44
Shawn <shawnfawcett@gmail.com>
2021-02-04 00:45:18 -0800

    medium level card count responsive

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import Tutorial from "./Components/Tutorial";
 //Import Styles
 import "./styles/app.scss";
 import { motion, AnimatePresence } from "framer-motion";
-//import useWindowSize from "./useWindowSize";
 
 function App() {
   const [pageStatus, setPageStatus] = useState(0);
-  // Can use this if responsive components (e.g. 1 for Mobile & 1 for Desktop)
-  //const windowSize = useWindowSize();
+
   return (
     <div className="App">
       <section>
